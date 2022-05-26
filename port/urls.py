7e4d6e878d3f17c069d252f5e3dda49c7100f3ed
Alvaro Boineau <alvaroboineau94@gmail.com>
2022-05-26 09:28:14 -0300

    a

--- port/urls.py
+++ port/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from port.views import indice
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indice),
 ]
---
+++ port/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+def indice(request):
+
+    return HttpResponse("SELECT YOUR LANGUAGE")
